1ec1dae3406e56cedbc0f7e2f4611d33c288d0c0
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-21 18:18:47 +0500

    Update BlogRollEdu.js

--- src/components/BlogRollEdu.js
+++ src/components/BlogRollEdu.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Link, graphql, StaticQuery } from "gatsby"
 import Image from "../components/image"
-import SideBlogRollEdu from "./side-Blogrolledu"
+import SideBlogRollEdu from "./side-BlogRollEdu"
 import addToMailchimp from "gatsby-plugin-mailchimp"
 
 const BlogRollEdu = props => {
